2e0b3aaa3ad741cd2ec4466e0084b05286d7549e
Szilágyi Róbert <szilagyirobert586@gmail.com>
2024-04-07 09:52:20 +0200

    2024.04.07

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Poke from './Poke';
 function App() {
+
+  const [result, setResult]=useState([]);
+  const [search, setSearch]=useState("");
+
+useEffect(()=>{
+axios.get("https://pokeapi.co/api/v2/pokemon")
+.then(res=>setResult(res.data.results));
+},[]);
+const pokeApi=result.filter(p=>{
+  return p.name.toLowerCase().includes(search.toLowerCase());
+});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input value={search} className='input' type='text' placeholder='Keresés' onChange={(e)=>setSearch(e.target.value)}/>
+
+    {
+      pokeApi.map((k)=>{
+        return <Poke name={k.name}/>
+      })
+    }
     </div>
+  
   );
+  
 }
 
 export default App;
---
+++ src/Poke.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Poke({ name }) {
+  return (
+<div>
+<div className='container'>
+      <p>{name}</p>
+    </div>
+</div>
+ 
+
+  )
+}
+
+export default Poke
